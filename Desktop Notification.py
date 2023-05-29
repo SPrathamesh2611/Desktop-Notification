5a84fe7c9a17904d64b9e1417518d0a8e28d2a3b
PRATHAMESH SURVE <134413659+SPrathamesh2611@users.noreply.github.com>
2023-05-29 22:50:09 +0530

    Update Desktop Notification.py
    comment added

--- Desktop Notification.py
+++ Desktop Notification.py
@@ -1,8 +1,12 @@
 from win10toast import ToastNotifier
 import datetime
 
+# Get the current date and time
 data = datetime.datetime.now()
 data = str(data)
+
+# Create a ToastNotifier instance
 toast = ToastNotifier()
 
-toast.show_toast("Date-Time Update",data,duration=15)
+# Display the desktop notification
+toast.show_toast("Date-Time Update", data, duration=15)
